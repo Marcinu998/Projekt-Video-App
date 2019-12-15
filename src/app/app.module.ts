e0f705f89cd61ccd58adbb7bfa6d9da29966cceb
Marcin Czarnocki <marcin_poczta1988@o2.pl>
2019-12-15 21:07:51 +0100

    add my-movies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { MyMoviesComponent } from './my-movies/my-movies.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyMoviesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/my-movies/my-movies.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-my-movies',
+  templateUrl: './my-movies.component.html',
+  styleUrls: ['./my-movies.component.css']
+})
+export class MyMoviesComponent implements OnInit {
+
+  message = new TemplateMessage();
+  sorts = ['- - -', 'Najstarsze filmy', 'Ostatnio dodane filmy'];
+
+  constructor() { }
+
+  ngOnInit() { }
+
+}
+
+class TemplateMessage {
+  constructor(
+    public sort = '',
+  ) { }
+}
