e688ad843e6bf0157d0e256d2372728a3505046f
Marcin Czarnocki <marcin_poczta1988@o2.pl>
2019-12-16 15:21:47 +0100

    add favorite-movies

--- src/app/favorite-movies/favorite-movies.component.ts
+++ src/app/favorite-movies/favorite-movies.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-favorite-movies',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoriteMoviesComponent implements OnInit {
 
+  @Input()
+  tasksFavorite: Array<string> = [];
+  @Output()
+  emitRemove = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  remove(task: string) {
+    this.emitRemove.emit(task);
   }
 
 }
