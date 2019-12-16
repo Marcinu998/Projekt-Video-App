efa13b4a60b812515ad74c00643a176d3d3fcac3
Marcin Czarnocki <marcin_poczta1988@o2.pl>
2019-12-16 14:52:18 +0100

    add movie-list

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-movie-list',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieListComponent implements OnInit {
 
+  @Input()
+  films = [];
+  @Output()
+  emitRemove = new EventEmitter<string>();
+  @Output()
+  emitFavorite = new EventEmitter<string>();
+
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  remove(task: string) {
+    this.emitRemove.emit(task);
+  }
+  favorite(task: string) {
+    this.emitFavorite.emit(task);
+  }
 }
