a95e7efaccda810bda3398606d93a4fc3c2424fa
Marcin Czarnocki <marcin_poczta1988@o2.pl>
2020-01-17 10:30:44 +0100

    newFilm object 2

--- src/app/movie-search/movie-search.component.ts
+++ src/app/movie-search/movie-search.component.ts
@@ -11,21 +11,12 @@ export class MovieSearchComponent implements OnInit {
   @Output()
   emitFilm = new EventEmitter();
 
-
-
-
   constructor() { }
 
   ngOnInit() {
   }
 
   add() {
-    // const film = { ...this.newFilm, createdAt: new Date().toLocaleDateString() };
-    // console.log(this.createdAt);
-    // const film = { createdAt: new Date().toLocaleDateString() };
-    // console.log(this.createdAt);
-    // this.createdAt.toLocaleDateString();
-    // console.log(this.createdAt);
     this.emitFilm.emit(this.newFilm);
     this.newFilm = [];
     console.log(this.emitFilm);
